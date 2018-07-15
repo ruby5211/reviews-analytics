59434e24c09e2cda19eaf50eee18305cc23bb804
Ruby Shih <ruby923@gmail.com>
2018-07-15 13:38:58 +0800

    add filtering2

--- review.py
+++ review.py
@@ -9,15 +9,23 @@ with open('reviews.txt', 'r') as f:
 print('檔案讀取完了，總共有', len(data), '筆資料')
 
 sum_len = 0
-for d in data: # d 是 data清單中的每一筆流言
+for d in data: # d 是 data清單中的每一筆留言
 	sum_len = sum_len + len(d)
 print('每筆留言的平均長度是', sum_len / len(data))
 
 # 篩選資料
 new = []
 for d in data:
-    if len(d) < 100:
-    	new.append(d)
+	if len(d) < 100:
+		new.append(d)
 print('一共有', len(new), '筆資料長度小於100')
 print(new[0])
+
+# 篩選資料2
+good = []
+for d in data:
+    if 'good' in d:
+    	good.append(d)
+print('一共有', len(good), '筆留言提到good')
+print(good[0])
 
