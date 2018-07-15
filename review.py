cc3339a02aaaac31bf042d2afbef8fd3ead1119c
Ruby Shih <ruby923@gmail.com>
2018-07-15 13:11:08 +0800

    clac avg len of reviews

--- review.py
+++ review.py
@@ -6,9 +6,13 @@ with open('reviews.txt', 'r') as f:
         count +=1
         if count % 10000 == 0: # 「％」求餘數
         	print(len(data))
-        }
-print(len(data))
+print('檔案讀取完了，總共有', len(data), '筆資料')
 
-print(data[0])
-print('-------')
-print(data[1])
+sum_len = 0
+for d in data: 
+	sum_len = sum_len + len(d)
+	
+print('每筆留言的平均長度是', sum_len / len(data))
+
+
+
